ea41461a2fea3b83e3203fd1a90d7899f3e7fe29
petersamson-kingori <peters.kingori@student.moringaschool.com>
2022-12-16 16:14:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here!
+
+// const element = document.createElement("div");
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+
+// element.append(ul);
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "YOUR-NAME is the champion"
+document.body.append(newHeader);
